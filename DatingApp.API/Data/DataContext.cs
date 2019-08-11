c3cb7decd4dee8e46fd2c4d9281b33d4eb390f99
Oxydius4 <oxydius4@gmail.com>
2019-08-12 01:39:05 +0200

    Build DTO

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext: DbContext
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options){}
-        public DbSet<Value> Values {get;set;}
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
